238a3cc7176c59f428aa119298c80c8bbee5595b
SBViside <ohvatuki@gmail.com>
2022-05-21 22:18:47 +0300

    burger-menu dev & ref

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 'use strict'
 
-const navigator = document.getElementsByClassName('navigator__list')[0];
-const burgerButton = document.getElementById('menu-burger');
-burgerButton.addEventListener('change', function() {
-    navigator.classList.toggle('burger-select');
-});
+// получение необходимых компонентов для бургер-меню
+    const navigator = document.getElementsByClassName('navigator__list')[0];
+    const navBackground = document.querySelector('.navigator-area');
+    const burgerButton = document.getElementById('menu-burger');
+
+// добавление событий бургер-меню
+    burgerButton.addEventListener('change', function() {
+        navigator.classList.toggle('burger-select');
+        navBackground.classList.toggle('dn');
+    });
+    navBackground.addEventListener('click', function() {
+        burgerButton.click();
+    });
